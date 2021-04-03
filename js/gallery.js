95813ce9e6ada421f20f535f2c74ab35daa72d67
Vladislav199026 <vladislaviakuba@gmail.com>
2021-04-03 19:40:31 +0300

    Brandy

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+	document.addEventListener( 'DOMContentLoaded', function () {
+        // new Splide('.splide').mount();
+        new Splide( '.splide', {
+	direction: 'ttb',
+	height   : '100vh'
+} ).mount();
+        
+    });
+    
+    
